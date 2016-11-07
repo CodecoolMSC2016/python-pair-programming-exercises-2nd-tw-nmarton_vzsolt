175ab35156b6cc51092576c7e98a7aea8cf47b6f
Vajni <vajnork.zsolt@gmail.com>
2016-11-07 17:29:54 +0100

    Zsolt commit

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,8 +1,33 @@
+import random
+import string
+
 def passwordgen():
-    return
+    user_input = input("Would you like a strong os weak password? :")
+    my_pass = []
+    symbol_list = ["@", "?", "!", "%", "+", ">", "#", "^", "&" , "*", "(", ")"]
+    if user_input == "strong":
+        for i in range (4):
+            my_pass.append(str(random.randint(1, 9)))
+            my_pass.append (random.choice(string.ascii_letters))
+            my_pass.append(random.choice(symbol_list))
+        print("Your password is: " + "".join(my_pass))
+        my_pass = str(my_pass)
+        return (my_pass)
+    else:
+        mypass_easy = []
+        easy = ["Easypass", "Youarehacked", "Lostdata", "Wikileak", "Busted", "Youarescrewed"]
+        mypass_easy.append(random.choice(easy))
+        print("Your password is: " + mypass_easy[0])
+        mypass_easy = str(mypass_easy)
+        
+    return mypass_easy
 
 
 def main():
+    
+    
+    passwordgen()
+
     return
 
 
--- years/years_module.py
+++ years/years_module.py
@@ -2,10 +2,17 @@ import datetime
 
 
 def years(age):
-    return
+    age_diff = 100 - age
+    today = datetime.date.today()
+    return int(today.year) + age_diff
 
 
 def main():
+    user_input = input("Please give me your name: ")
+    user_input_2 = int(input("Please give me your age: "))
+    print("Welcome " + user_input)
+    years(user_input_2)
+    print("Dear " + user_input + ", you will turn 100 in " + str(years(user_input_2)))
     return
 
 
