8a1083cd0a9e4a45a3a211a213de48572aa12dd2
Vajni <vajnork.zsolt@gmail.com>
2016-11-07 14:26:04 +0100

    Zsolt commit

--- listoverlap/listoverlap_module.py
+++ listoverlap/listoverlap_module.py
@@ -1,8 +1,19 @@
+import random
+
 def listoverlap(list1, list2):
-    return
+    set_list1 = set(list1)
+    set_list2 = set(list2)
+    list3 = set_list1 & set_list2
+    print(list3)
+    return list(list3)
 
 
 def main():
+    list1 = [random.randint(1,10) for i in range(10)]
+    list2 = [random.randint(1,20) for i in range(20)]
+
+    print(list1, list2)
+    listoverlap(list1, list2)
     return
 
 
